20a5b4480f686123146e28673cadbd83cc1542fa
SantiagoAlvarez <salvarez@lean-tech.io>
2020-05-01 17:16:06 -0500

    creating the state for the expenses

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+
+    const [nombre, setNombre] = useState('');
+    const [cantidad, setCantidad] = useState(0);
+
+    //Cuando el usuario agrega un gato 
+    const agregarGasto = e => {
+        e.preventDefault()
+
+        //Validar
+
+
+        //construir el gasto 
+
+
+        //pasart gasto al componente principal 
+
+        //resetear el form
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={agregarGasto}
+        >
             <h2>Agrega tus datos aqui</h2>
             <div className="campo">
                 <label>Nombre Gasto</label>
@@ -10,14 +31,18 @@ const Formulario = () => {
                     type="text"
                     className="u-full-width"
                     placeholder="Ej. Transporte"
+                    value={nombre}
+                    onChange={e => setNombre(e.target.value)}
                 />
             </div>
             <div className="campo">
-                <label>Catidad Gasto</label>
+                <label>Cantidad Gasto</label>
                 <input
                     type="number"
                     className="u-full-width"
                     placeholder="Ej. 300"
+                    value={cantidad}
+                    onChange={e => setCantidad(parseInt(e.target.value))}
                 />
             </div>
             <input
