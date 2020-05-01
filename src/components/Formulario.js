24f5b7d4dae8045da62e074819400eec1e98bb72
SantiagoAlvarez <salvarez@lean-tech.io>
2020-05-01 16:49:46 -0500

    Creating expense form

---
+++ src/components/Formulario.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Formulario = () => {
+    return (
+        <form>
+            <h2>Agrega tus datos aqui</h2>
+            <div className="campo">
+                <label>Nombre Gasto</label>
+                <input
+                    type="text"
+                    className="u-full-width"
+                    placeholder="Ej. Transporte"
+                />
+            </div>
+            <div className="campo">
+                <label>Catidad Gasto</label>
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Ej. 300"
+                />
+            </div>
+            <input
+                type="submit"
+                className="button-primary u-full-width"
+                value="Agregar Gasto"
+            />
+        </form>
+    );
+}
+
+export default Formulario;
